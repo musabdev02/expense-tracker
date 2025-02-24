f35c372aa5b849907a1db721c8ed2779bb290d55
MusabLodhi <musabdev65@gmail.com>
2025-02-24 20:54:02 +0500

    add name functionality with localstorage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,61 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+// components
 import Button from '../components/UI/Button'
 import Card from '../components/Home/Card'
 import TransactionsContainer from '../components/Home/TransactionsContainer'
+
 const Home = () => {
+  const [userName, setUserName] = useState("");
+  const [isUserPopup, setIsUserPopup] = useState(false)
+  useEffect(() => {
+    const name = localStorage.getItem("name");
+    if(name){
+      setUserName(name)
+      setIsUserPopup(false)
+    }else{
+      setIsUserPopup(true)
+    }
+  }, [])
+  // saveName
+  const saveName = () => {
+    if(userName !== ""){
+      localStorage.setItem("name", userName);
+      setUserExist(false)
+    }
+  }
     return (
+      <>
+      {/* name Popup */}
+      {
+        isUserPopup && <div className="w-full h-screen z-9 left-0 top-0 absolute bg-[#020202eb] flex items-center justify-center">
+        <div className="w-[20%] h-[200px] bg-white rounded-lg p-8 text-center">
+          <h3 className=" text-xl text-zinc-600">
+            Enter Your Name to Continue
+          </h3>
+          <input
+            type="text"
+            placeholder="Your Name..."
+            className="capitalize border border-zinc-300 w-full px-4 py-3 rounded-lg outline-none mt-4 focus:border-blue-300"
+            spellCheck={false}
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+          />
+          <button
+            className="mt-2 border border-blue-300 px-4 py-2 rounded-lg cursor-pointer hover:bg-blue-100"
+            onClick={saveName}
+          >
+            Get Started
+          </button>
+        </div>
+      </div>
+      }
+        
+      {/* homePage */}
         <section>
-          {/* home content */}
           <div className="flex items-center justify-between">
             <div>
               <h3 className="font-semibold text-4xl text-blue-900">
-                Hey there, {"musab".slice(0, 1).toUpperCase() + "musab".slice(1)}
+                Hey there, {userName.slice(0, 1).toUpperCase() + userName.slice(1)}
               </h3>
               <p className="pt-2 text-zinc-700 text-md">
                 Have a great day! Good luck on your finanical journey.
@@ -41,7 +87,7 @@ const Home = () => {
             <h4 className="mb-6 text-lg text-zinc-500">News & Information</h4>
             {/* add balance card! */}
             {
-              1 > 1 ? "Nothing here.." : 
+              0 > 1 ? "Nothing here.." : 
               <div className="w-[300px] h-52 bg-yellow-200 rounded-md p-8">
                 <h3 className="font-[500] text-xl text-yellow-900 leading-7">Add a Balance to Get Started Your Finanical Journey.</h3>
                 <p className="mt-8 font-semibold cursor-pointer text-yellow-800 hover:underline">Add Balance</p>
@@ -50,6 +96,7 @@ const Home = () => {
           </div>
           </section>
         </section>
+      </>
       );
 }
 
