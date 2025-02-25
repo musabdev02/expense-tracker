a73c1cd8a2ea49968df57f85e433c93aede92012
MusabLodhi <musabdev65@gmail.com>
2025-02-25 21:32:33 +0500

    make sidebar responsive for small devices

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,28 +9,28 @@ const Sidebar = () => {
   let location = useLocation();
 
   return (
-    <section className="hidden sm:block w-[16%] h-screen bg-[#f0f7ff]">
-      <div className="py-20">
-        <div className="logo px-12 flex items-center gap-2">
+    <section className="w-[100%] sm:w-[16%] sm:static z-9 fixed bottom-0 sm:h-screen bg-[#f0f7ff]">
+      <div className="sm:py-20">
+        <div className="hidden sm:flex logo px-12 items-center gap-2">
           <img src="/logo.png" alt="logo" className="w-10" />
           <h3 className="font-bold text-xl tracking-tight leading-5">Spend <br /> Wise</h3>
         </div>
-        <div className="flex flex-col gap-4 mt-12 text-center px-6">
-            <Link to={"/"} className={`cursor-pointer flex items-center gap-4 ${location.pathname === "/" ? "bg-blue-300 hover:bg-blue-300" : ""} hover:bg-blue-100 p-3 rounded-lg`}>
-                <span><IoHomeOutline size={22} /></span>
-                <p className="text-lg">Home</p>
+        <div className="flex justify-between flex-row flex-wrap sm:flex-col gap-4 sm:mt-12 mt-4 mb-4 sm:mb-0 text-center px-6">
+            <Link to={"/"} className={`cursor-pointer flex flex-col sm:flex-row items-center gap-4 ${location.pathname === "/" ? "bg-blue-300 hover:bg-blue-300" : ""} hover:bg-blue-100 p-3 rounded-lg`}>
+                <span className="text-2xl sm:text-md"><IoHomeOutline /></span>
+                <p className="text-lg hidden sm:block">Home</p>
             </Link>
-            <Link to={"/categories"} className={`cursor-pointer flex items-center hover:bg-blue-100 gap-4 p-3 rounded-lg ${location.pathname === "/categories" ? "bg-blue-300 hover:bg-blue-300" : ""}`}>
-                <span><MdOutlineCategory size={22} /></span>
-                <p className="text-lg">Categories</p>
+            <Link to={"/categories"} className={`cursor-pointer flex-col sm:flex-row flex items-center hover:bg-blue-100 gap-4 p-3 rounded-lg ${location.pathname === "/categories" ? "bg-blue-300 hover:bg-blue-300" : ""}`}>
+                <span className="text-2xl sm:text-md"><MdOutlineCategory /></span>
+                <p className="text-lg hidden sm:block">Categories</p>
             </Link>
-            <Link to={"/statistics"}className={`cursor-pointer flex items-center hover:bg-blue-100 gap-4 p-3 rounded-lg ${location.pathname === "/statistics" ? "bg-blue-300 hover:bg-blue-300" : ""}`}>
-                <span><FaRegChartBar size={22} /></span>
-                <p className="text-lg">Statistics</p>
+            <Link to={"/statistics"}className={`cursor-pointer flex-col sm:flex-row flex items-center hover:bg-blue-100 gap-4 p-3 rounded-lg ${location.pathname === "/statistics" ? "bg-blue-300 hover:bg-blue-300" : ""}`}>
+                <span className="text-2xl sm:text-md"><FaRegChartBar /></span>
+                <p className="text-lg hidden sm:block">Statistics</p>
             </Link>
-            <Link to={"/settings"} className={`cursor-pointer flex items-center hover:bg-blue-100 gap-4 p-3 rounded-lg ${location.pathname === "/settings" ? "bg-blue-300 hover:bg-blue-300" : ""}`}>
-                <span><IoSettingsOutline size={22} /></span>
-                <p className="text-lg">Settings</p>
+            <Link to={"/settings"} className={` cursor-pointer flex flex-col sm:flex-row items-center hover:bg-blue-100 gap-4 p-3 rounded-lg ${location.pathname === "/settings" ? "bg-blue-300 hover:bg-blue-300" : ""}`}>
+                <span className="text-2xl sm:text-md"><IoSettingsOutline /></span>
+                <p className="text-lg hidden sm:block">Settings</p>
             </Link>
         </div>
       </div>
